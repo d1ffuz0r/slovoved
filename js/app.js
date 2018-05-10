759da7781b3fc923305bb294650ca031845d5c44
Roman Gladkov <d1fffuz0r@gmail.com>
2018-05-09 22:39:40 -0700

    удалил изображения, добавил подсчёт показателя чистоты текста

--- js/app.js
+++ js/app.js
@@ -1,16 +1,6 @@
 var output_div = $(".words");
 var stats_div = $(".stats");
 
-function findAlternative(word) {
-    var alternative = null;
-    for (suggestion of words) {
-        if (word.toLowerCase().trim().indexOf(suggestion[0]) !== -1) {
-            return suggestion;
-        };
-    }
-    return null;
-}
-
 function replaceWord(word, alternative) {
     return '<span style="color: red" title="' + word + ' -> ' + alternative + '"> '+ word + '</span>';
 }
