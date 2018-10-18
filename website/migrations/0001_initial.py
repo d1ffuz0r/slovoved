db76cfe48d857be90d5837a238bfd4e030c63a6d
Гладков Роман <roman@cyberkolhoz.ru>
2018-10-17 20:35:08 -0700

    добавил в миграцию включения расширения pg_tgrm

--- website/migrations/0001_initial.py
+++ website/migrations/0001_initial.py
@@ -20,4 +20,5 @@ class Migration(migrations.Migration):
                 ('active', models.BooleanField(default=True)),
             ],
         ),
+        migrations.RunSQL('CREATE EXTENSION IF NOT EXISTS pg_tgrm;'),
     ]
