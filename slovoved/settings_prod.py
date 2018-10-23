87f96f33fc71a177d7920e6547fa42cb06a6b4d0
Гладков Роман <roman@cyberkolhoz.ru>
2018-10-22 23:49:52 -0700

    добален sentry

--- slovoved/settings_prod.py
+++ slovoved/settings_prod.py
@@ -1,4 +1,13 @@
 from slovoved.settings import *
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 DEBUG = False
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
+
+sentry_sdk.init(
+    dsn=os.environ.get("SENTRY_DSN"),
+    integrations=[DjangoIntegration()]
+)
