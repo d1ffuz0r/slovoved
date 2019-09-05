d2e7dc64e8a55b78ba8e14da7e8c97ba857f4fea
Roman Gladkov <d1fffuz0r@gmail.com>
2019-09-05 10:40:16 +0300

    добавил файлы для запуска процессов

---
+++ extra/run_celeryd.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+NAME="slovoved"
+DJANGODIR=/var/www/slovoved/                       # Django project directory
+DJANGO_SETTINGS_MODULE=slovoved.settings_prod      # which settings file should Django use
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source ../../venv/bin/activate
+
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=slovoed
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+exec celery worker \
+    -A slovoved \
+    -O fair \
+    -l INFO
---
+++ extra/run_scheduler.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+NAME="slovoved"
+DJANGODIR=/var/www/slovoved/                       # Django project directory
+DJANGO_SETTINGS_MODULE=slovoved.settings_prod      # which settings file should Django use
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source ../../venv/bin/activate
+
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=slovoed
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+exec celery beat \
+    -A slovoved \
+    -l INFO
